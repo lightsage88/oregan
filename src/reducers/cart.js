0ec1d073049dace79e77e8090e961d287be527f0
lightsage88 <adrian.e.rosales@gmail.com>
2018-04-14 02:06:38 -0700

    3rd c

---
+++ src/reducers/cart.js
@@ -0,0 +1,9 @@
+const initialState = {
+	cart: []
+}
+
+const cart = (state=initialState, action) => {
+	return state
+}
+
+export default cart
---
+++ src/reducers/user.js
@@ -0,0 +1,16 @@
+const initialState = {
+	token: '',
+	firstName: '',
+	lastName: '',
+	emailAddress: '',
+	phoneNumber: '',
+	_id: '',
+	username: '',
+	validLogin: ''
+}
+
+const user = (state=initialState, action) => {
+	return {}
+}
+
+export default user
