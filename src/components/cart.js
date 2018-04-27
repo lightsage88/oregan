7a244994d96540acafa465b05dcd9ebdcb3e5d5a
lightsage88 <adrian.e.rosales@gmail.com>
2018-04-27 15:04:05 -0700

    cart taking shape

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import ProductCard from './productCard';
+import CartItem from './cartItem';
 export default class Cart extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			carty:''
-		}
+			currentCart: ''
+		};
 	}
+
+	componentWillReceiveProps(nextProps){
+		console.log(this.props);
+		console.log(nextProps);
+	
+		console.log(nextProps.currentCart);
+		let currentCart = nextProps.currentCart;
+		this.setState({
+			currentCart: currentCart
+		});
+		// return currentCart;
+	}
+
+
 	
 
 	render(){
-		console.log(this.props);
+		console.log(this.state);
+		let currentCart = this.state.currentCart;
+		console.log(currentCart);
+		const inventory = Object.values(currentCart);
+		const items = inventory.map((item,index)=>
+			<div key={index}>
+				<CartItem details={item}/>
+			</div>
+			);
 		return (
 			<div>
 				<h3>Cart</h3>
+				{items}
 			</div>
 			);
 	}
---
+++ src/components/cartItem.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import {Card, 
+	CardImg, 
+	CardText, 
+	CardBody,
+  CardTitle, 
+  CardSubtitle, 
+  CardLink, 
+  Button,
+Form,
+FormGroup,
+Label,
+Input} from 'reactstrap';
+
+import {connect} from 'react-redux';
+import {retrieveProducts} from '../actions/index';
+
+import {putItemInCart2} from '../actions/index';
+import {putItemInCart1} from '../actions/index';
+export class CartItem extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			quantityOrdered: 0,
+			companyName: '',
+			productName: '',
+			productDescription: '',
+			productRating: '',
+			productPrice: '',
+			shippingPrice:'',
+			productStock:'',
+			productType: '',
+			id: '',
+			quantityUnavailable: false
+
+		}
+	}
+
+	onChange(e){
+		console.log('onChange running');
+		console.log(e.target.value);
+		this.setState({
+			quantityOrdered : e.target.value,
+			companyName: this.props.details.companyName,
+			id: this.props.details.id,
+			productDescription: this.props.details.productDescription,
+			productName: this.props.details.productName,
+			productPrice: this.props.details.productPrice,
+			shippingPrice: this.props.details.shippingPrice,
+			productRating: this.props.details.productRating,
+			productStock: this.props.details.productStock,
+			productType: this.props.details.productType
+		});
+		if(e.target.value > this.props.details.productStock) {
+			console.log('too much, hooman');
+			this.setState({
+				quantityUnavailable: true
+			});
+		} else {
+			this.setState({
+				quantityUnavailable: false
+			});
+		}
+
+	}
+
+	addToCart(e){
+		e.preventDefault();
+		let cart = this.props.currentCart;
+
+		let dateToCart = new Date;
+		let cartLength = cart.length;
+		let pageType = this.props.pageType;
+		let userid= localStorage.getItem('_id');
+		let quantityOrdered = this.state.quantityOrdered;
+		let companyName = this.props.details.companyName;
+		let id = this.props.details.id;
+		let productDescription= this.props.details.productDescription;
+		let productName= this.props.details.productName;
+		let productPrice= this.props.details.productPrice;
+		let shippingPrice= this.props.details.shippingPrice;
+		let productRating= this.props.details.productRating;
+		let productStock= this.props.details.productStock;
+		let productType= this.props.details.productType;
+
+		let item = {quantityOrdered,
+			dateToCart, 
+			companyName,
+			id,
+			productDescription,
+			productName,
+			productPrice,
+			shippingPrice,
+			productRating,
+			productStock,
+			productType
+		};
+
+		console.log(cart);
+		console.log(item);
+		if(cart.length === 0){
+			cart.push(item)
+		}
+
+
+	if(cart.length > 0) {
+		for(let i = 0; i<=cart.length-1; i++){
+			console.log('cycling thru, we dont play');
+			if(cart[i].id === item.id) {
+				console.log(cart[i]);
+				console.log(item);	
+				cart.splice(i, 1);
+				console.log(cart);			
+			}
+		}
+		cart.push(item);
+		console.log('newcart?');
+		console.log(cart);
+	} else {
+		console.log('NEWB');
+		cart.push(item);
+		console.log(cart);
+
+	}
+		
+
+	// this.props.dispatch(putItemInCart1(cart, cartLength, pageType, userid, quantityOrdered, companyName, id, productDescription,productName,productPrice, shippingPrice, productRating, productStock, productType));
+	this.props.dispatch(putItemInCart1(cart, userid, pageType));		
+
+}
+
+removeFromCart(e){
+	console.log('removeFromCart running...');
+	e.preventDefault();
+	console.log(e);
+}
+
+
+	render(){
+		// let quantityChoice = this.state.quantityOrdered;
+		// let productInfo = this.state.product;
+		// console.log(quantityChoice);
+		return (
+		<div>
+			<Card>
+				<CardBody>
+					<CardTitle>{this.props.details.productName}</CardTitle>
+					<CardSubtitle>{this.props.details.companyName}</CardSubtitle>
+					<CardImg src='../staticAssets/shoppingCard.png'/>
+					<CardText>
+						{this.props.details.productDescription}
+					</CardText>
+					<ul>
+							<li>COST: <span>{this.props.details.productPrice}</span></li>
+							<li>SHIPPING: <span>{this.props.details.shippingPrice}</span></li>
+						</ul>
+					<Form>
+						<FormGroup>
+						<Label for='quantity'>Qty: </Label>
+						<Input onChange={(e)=>this.onChange(e)} id='quantity' name='quantity' placeholder={this.props.details.quantityOrdered} min=
+						'1' max={this.props.details.productStock} type='number'></Input>
+						
+						</FormGroup>
+						{
+						!this.state.quantityUnavailable ? 
+						(<Button onClick={(e)=>this.addToCart(e)}>edit quantity</Button>)
+						:
+						(<Button disabled>unavailable</Button>)
+						}
+						<Button color='danger' onClick={(e)=>this.removeFromCart(e)}>remove item</Button>
+					</Form>
+				</CardBody>
+			</Card>
+		</div>
+	);
+ }
+}
+
+const mapStateToProps = state => ({
+	currentCart : state.app.user.cart
+});
+export default connect(mapStateToProps)(CartItem);
