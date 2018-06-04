632e86dcc7a57bb083c04d0bd2d01e7409c66f39
Adrian Rosales <adrian.e.rosales@gmail.com>
2018-06-04 12:08:57 -0700

    going live

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
 	DATABASE_URL: process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://lightsage88:Walruses8@ds149309.mlab.com:49309/oregan',
 	JWT_SECRET: process.env.JWT_SECRET || 'pepperTheCat',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '15m',
-	API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080" 
+	API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://protected-bastion-38106.herokuapp.com" 
 };
 
 // "https://protected-bastion-38106.herokuapp.com" "http://localhost:8080"
